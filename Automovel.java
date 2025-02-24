1d5b98564bc1be00cc73f5dedbfcedd9ab7b08bc
anabelaPereira440 <up202406638@up.pt>
2025-02-24 21:42:12 +0000

    Eliminei os comentários da classe main e da classe Automovel.

--- Automovel.java
+++ Automovel.java
@@ -1,27 +1,23 @@
 public class Automovel {
-    //atributos de instância, característica de cada um dos objetos
     private String matricula;
     private String marca;
     private int cilindrada;
 
-    private static final String MATRICULA_POR_OMISSAO = "sem matrícula";//é uma constante(final). é um atributo de classe(static)
+    private static final String MATRICULA_POR_OMISSAO = "sem matrícula";
     private static final String MARCA_POR_OMISSAO = "sem marca";
     private static final int CILINDRADA_POR_OMISSAO = 0;
 
-    private static int totalAutomoveis = 0;//contador. variável de classe(static)
+    private static int totalAutomoveis = 0;
 
-    public Automovel() {//construtor-só tem public-são metodos especiais//nome do construtor SEMPRE igual ao da classe. é um construtor vazio!!!
+    public Automovel() {
         matricula = MATRICULA_POR_OMISSAO;
-        this.marca = MARCA_POR_OMISSAO;//this ou não é igual!!!
+        this.marca = MARCA_POR_OMISSAO;
         cilindrada = CILINDRADA_POR_OMISSAO;
-        totalAutomoveis++;//contador de instância. sempre que se cria um objeto este contador acrescenta +1//não diz respeito a nenhum objeto em concreto
+        totalAutomoveis++;
     }
 
-    //public Automovel(){} //construtor vazio
-    //dependendo do número de variáveis passadas vão ser chamados diferentes construtores//estes construtores e as variáveis são passadas através do main
-
-    public Automovel(String matricula, String marca, int cilindrada){ //construir o objeto //construtor completo
-        this.matricula=matricula;//o this.nome diz respeito ao atributo//o nome é o nome que foi passado por parametro//nome=nome ele fica confuso
+    public Automovel(String matricula, String marca, int cilindrada){
+        this.matricula=matricula;
         this.marca=marca;
         this.cilindrada=cilindrada;
         totalAutomoveis++;
@@ -69,10 +65,10 @@ public class Automovel {
         return cilindrada;
     }
     public void setMatricula(String matricula) {
-        this.matricula=matricula;//evita ambiguidades
+        this.matricula=matricula;
     }
     public void setMarca(String marca) {
-        this.marca=marca;// o "this" evita ambiguidades
+        this.marca=marca;
     }
     public void setCilindrada(int cilindrada) {
         this.cilindrada=cilindrada;
@@ -81,26 +77,22 @@ public class Automovel {
 
     public String toString() {
         return String.format("O automóvel com matrícula %s é um %s e tem cilindrada de %d cc. ", matricula,marca,cilindrada);
-        //existe uma alternativa menos eficiente
     }
 
     public int calcularDiferencaCilindrada(Automovel outroAutomovel) {
-        return this.cilindrada- outroAutomovel.cilindrada;//parametro implicito-parametro explicito
+        return this.cilindrada- outroAutomovel.cilindrada;
     }
 
-    public boolean isCilindradaMaior(Automovel outroAutomovel) {//passar objetos por parametro
+    public boolean isCilindradaMaior(Automovel outroAutomovel) {
         if (this.cilindrada> outroAutomovel.cilindrada){
             return true;
         }else{
             return false;
         }
-        //return this.cilindrada>outroAutomovel.cilindrada
-    }
-    /*public boolean isCilindradaMaior(int cilindrada){
-        return this.cilindrada>cilindrada;
-    }*/ //exemplo que tenho entre asteriscos
 
-    public static int getTotalAutomoveis() { //metodo static. não está associado a nenhum objeto em concreto
+    }
+
+    public static int getTotalAutomoveis() {
         return totalAutomoveis;
     }
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Automovel a1=new Automovel("11-11-AA","Toyota",1400);//criação 1ªinstancia
+        Automovel a1=new Automovel("11-11-AA","Toyota",1400);
         System.out.println(a1.toString());
         System.out.print("A matrícula do automóvel a1 é "+a1.getMatricula());
         System.out.print("\nNúmero de instâncias criadas até ao momento: "+ Automovel.getTotalAutomoveis());
 
         Automovel a2=new Automovel("22-22-BB","Fiat");//criação 2ªinstância
         System.out.print("\n"+a2.toString());
-        a2.setCilindrada(1800);//registo de uma nova matricula. substitui o que lá está por uma coisa nova
+        a2.setCilindrada(1800);
         System.out.print("\n"+a2.toString());
         System.out.print("\nNúmero de instâncias criadas até ao momento: "+ Automovel.getTotalAutomoveis());
 
 
         System.out.print("\nA diferença de cilindrada (valor absoluto) entre os automóveis a1 e a2 é: "+ a1.calcularDiferencaCilindrada(a2)+" cc.");
-        boolean comparacao= a1.isCilindradaMaior(a2);//*******a1.isCilindradaMaior(a2.getCilindrada)******;
+        boolean comparacao= a1.isCilindradaMaior(a2);
         if (comparacao){
             System.out.print("\nA matrícula com a cilindrada maior é "+ a1.getMatricula());
         }else{
